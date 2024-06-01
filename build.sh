252fa98c12ff53ad9245d7b8b80f33a9f3daa223
Jaehee Lee <55607202+ij5@users.noreply.github.com>
2024-06-01 19:04:15 +0900

    Auto Commit

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ mkdir -p content/posts
 rm -r public/*
 rm -r content/posts/*
 obsidian-export ../Blog/ content/posts/ --frontmatter always
-hugo --minify
 git add .
 git commit -m "Auto Commit"
 git push
